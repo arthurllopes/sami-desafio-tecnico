b845602fbb266fcd1d3940ccd23a7a55f4956e13
Arthur Lopes <arthurllopes10@gmail.com>
2021-10-19 20:07:20 -0300

    final commit

--- src/components/HeroesList/index.js
+++ src/components/HeroesList/index.js
@@ -6,9 +6,10 @@ import styles from './index.module.scss'
 const HeroesList = () => {
     const {data, loading} = useSelector(state => state.items)
     if (loading) return <p>Carregando...</p>
+    const sliced = data.slice(0, 24)
     return (
         <ul className={styles.HeroesContent}>
-            {data?.map((item) => (
+            {sliced?.map((item) => (
                 <HeroItem key={item.id} item={item}/>
             ))}
         </ul>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Header from '../../components/Header'
 import HeroesList from '../../components/HeroesList'
 import { fetchItems } from '../../store/Items'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Modal from '../../components/ModalItem'
 
 const HomePage = () => {
